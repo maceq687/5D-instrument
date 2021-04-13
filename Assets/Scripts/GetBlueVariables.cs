a7b15ca8de0c3fb6d15b62996c4b10bc05379def
maceq687 <maciek@softman.pl>
2021-04-13 17:59:24 +0200

    WIP multiplayer - audio engine connection

---
+++ Assets/Scripts/GetBlueVariables.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetBlueVariables : MonoBehaviour
+{
+    public float XBlue;
+    public float YBlue;
+    bool xWithin = false;
+    bool yWithin = false;
+    bool mouseWithinC = false;
+    float lastMouseX;
+    float lastMouseY;
+   
+    void Update()
+    {
+        float xRelative = (transform.localPosition.x + 42) / 84;
+        float yRelative = (transform.localPosition.y + 42) / 84;
+
+        if (xRelative > 0 && xRelative < 1 ) { xWithin = true; }
+        else { xWithin = false; }
+        if (yRelative > 0 && yRelative < 1 ) { yWithin = true; }
+        else { yWithin = false; }
+
+        float mouseX = Mathf.Clamp(Mathf.Round(xRelative * 127), 0, 127);
+        float mouseY = Mathf.Clamp(Mathf.Round(yRelative * 127), 0, 127);
+
+        float mouseXDelta = mouseX - lastMouseX;
+        float mouseYDelta = mouseY - lastMouseY;
+
+        if (xWithin == true && yWithin == true)
+            { 
+                mouseWithinC = true;
+            }
+        else
+            {
+                mouseWithinC = false;
+            }
+
+        if (mouseXDelta != 0 && mouseWithinC == true)
+            {
+                // Debug.Log("mouseX: " + mouseX);
+                XBlue = mouseX;
+            }
+        
+        if (mouseYDelta != 0 && mouseWithinC == true)
+            {   
+                // Debug.Log("mouseY: " + mouseY);
+                YBlue = mouseY;
+            }
+
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+    }
+}
--- Assets/Scripts/GetVariables.cs
+++ Assets/Scripts/GetVariables.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class GetVariables : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float xBlueOld;
+    float yBlueOld;
 
-    // Update is called once per frame
+    [DllImport("__Internal")]
+    private static extern void SetParamA(float paramA);
+
+    [DllImport("__Internal")]
+    private static extern void SetParamB(float paramB);
+
     void Update()
     {
-        
+        float xBlue = GameObject.Find("BotBlue").GetComponent<GetBlueVariables>().XBlue;
+        float yBlue = GameObject.Find("BotBlue").GetComponent<GetBlueVariables>().YBlue;
+
+        if (xBlue != xBlueOld) {
+            #if UNITY_WEBGL && !UNITY_EDITOR
+            SetParamA(xBlue);
+            #endif
+            // Debug.Log("xBlue: " + xBlue);
+        }
+
+        if (yBlue != yBlueOld) {
+            #if UNITY_WEBGL && !UNITY_EDITOR
+            SetParamB(yBlue);
+            #endif
+            // Debug.Log("yBlue: " + yBlue);
+        }
+        xBlueOld = xBlue;
+        yBlueOld = yBlue;
     }
 }
