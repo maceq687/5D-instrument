8830608ec882ecb52582a98f2906aaf53520f09b
maceq687 <maciek@softman.pl>
2021-04-23 20:19:48 +0200

    multiplayer - fixed assigning seats to new players

--- Assets/Scripts/Multi-player/GameManager.cs
+++ Assets/Scripts/Multi-player/GameManager.cs
@@ -2,87 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using ExitGames.UtilityScripts;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject PlayerPrefab;
     public GameObject Button;
     public GameObject SceneCamera;
-    // public PhotonView photonView;
-
-    // public bool SeatOneOccupation;
-    // public bool SeatTwoOccupation;
-    // public bool SeatThreeOccupation;
-    // public bool SeatFourOccupation;
-    // public bool SeatFiveOccupation;
-
-
+    public PhotonView photonView;
     Vector3[] list1 = new Vector3[5] {new Vector3(0, 0, 0), new Vector3( 185, 0, 130), new Vector3( -185, 0, 130), new Vector3( 116, 0, 355), new Vector3( -116, 0, 355)};
     Quaternion[] list2 = new Quaternion[5] {(Quaternion.identity), Quaternion.Euler(0, -73, 0), Quaternion.Euler(0, 73, 0), Quaternion.Euler(0, -144, 0), Quaternion.Euler(0, 144, 0)};
 
-    private void Awake()
+    void Awake()
     {
         Button.SetActive(true);
-        //PhotonNetwork.automaticallySyncScene = true; 
-        //photonView = GetComponent<PhotonView>();
+        PhotonNetwork.automaticallySyncScene = true; 
+        photonView = GetComponent<PhotonView>();
     }
 
     public void SpawnPlayer()
-    {
-        if (PhotonNetwork.countOfPlayers == 1)
-        {
-            PhotonNetwork.Instantiate(PlayerPrefab.name, list1[0], list2[0], 0);
-
-            // SeatOneOccupation = true;
-            // Debug.Log("SeatPilot");
-
-            Button.SetActive(false);
-            SceneCamera.SetActive(false);
-        }
-
-        else if (PhotonNetwork.countOfPlayers == 2)
-        {
-            PhotonNetwork.Instantiate(PlayerPrefab.name, list1[1], list2[1], 0);
-                    
-            // SeatTwoOccupation = true;
-            // Debug.Log("Seat2");
-
-            Button.SetActive(false);
-            SceneCamera.SetActive(false);
-        }
-
-        else if (PhotonNetwork.countOfPlayers == 3)
-        {
-            PhotonNetwork.Instantiate(PlayerPrefab.name, list1[2], list2[2], 0);
-                    
-            // SeatThreeOccupation = true;
-            // Debug.Log("Seat3");
-
-            Button.SetActive(false);
-            SceneCamera.SetActive(false);
-        }
-
-        else if (PhotonNetwork.countOfPlayers == 4)
-        {
-            PhotonNetwork.Instantiate(PlayerPrefab.name, list1[3], list2[3], 0);
-                    
-            // SeatFourOccupation = true;
-            // Debug.Log("Seat4");
-
-            Button.SetActive(false);
-            SceneCamera.SetActive(false);
-        }
-
-        else 
-        {
-            PhotonNetwork.Instantiate(PlayerPrefab.name, list1[4], list2[4], 0);
-
-            // Debug.Log("Seat5");
-
-            Button.SetActive(false);
-            SceneCamera.SetActive(false);
-        }
-        
+    {   
+        Debug.Log("Number of players: " + PhotonNetwork.countOfPlayers);
+        int seatNumber = PhotonNetwork.player.GetRoomIndex();
+        PhotonNetwork.Instantiate(PlayerPrefab.name, list1[seatNumber], list2[seatNumber], 0);
+        Button.SetActive(false);
+        SceneCamera.SetActive(false);
+        Debug.Log("Seat number: " + ++seatNumber);
     }
+
+    // void OnPhotonPlayerDisconnected(PhotonPlayer newPlayer)
+    // {
+    //     PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.player);
+    //     PhotonNetwork.LeaveRoom();
+    // }
 }
  
--- Assets/Scripts/Multi-player/MenuController.cs
+++ Assets/Scripts/Multi-player/MenuController.cs
@@ -6,55 +6,22 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private string VersioName = "0.1";
-    //[SerializeField] private GameObject UsernameMenu;
     [SerializeField] private GameObject ConnectPanel;
-
-    //[SerializeField] private InputField UsernameInput;
-    //[SerializeField] private InputField CreateGameInput;
-    //[SerializeField] private InputField JoinGameInput;
-
-    // [SerializeField] private GameObject StartButton;
 
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings(VersioName);
     }
 
-    //private void Start()
-    //{
-    //    UsernameMenu.SetActive(true);
-    //}
-
     private void OnConnectedToMaster()
     {
         PhotonNetwork.JoinLobby(TypedLobby.Default);
         Debug.Log("Connected");
     }
-    
-    // Username
-    //public void ChangeUserNameInput()
-    //{
-    //    if(UsernameInput.text.Length >= 3)
-    //    {
-    //        StartButton.SetActive(true);
-    //    }
-    //    else
-    //    {
-    //        StartButton.SetActive(false);
-    //    }
-    //}
-
-    //public void SetUserName()
-    //{
-    //    UsernameMenu.SetActive(false);
-    //    PhotonNetwork.playerName = UsernameInput.text;
-    //}
 
     public void CreateGame()
     {
-        //PhotonNetwork.CreateRoom(CreateGameInput.text, new RoomOptions() { maxPlayers = 5 }, null);
         PhotonNetwork.CreateRoom("basic", new RoomOptions() { MaxPlayers = 5, PublishUserId = true}, null);
-        
     }
 
 
@@ -63,17 +30,20 @@ public class MenuController : MonoBehaviour
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 5;
         roomOptions.PublishUserId = true;
-        //Debug.Log(PhotonPlayer.UserId);
-        //PhotonNetwork.JoinOrCreateRoom(JoinGameInput.text, roomOptions, TypedLobby.Default);
         PhotonNetwork.JoinOrCreateRoom("basic", roomOptions, TypedLobby.Default);
     }
 
     private void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("trial");
-        string test = PhotonNetwork.AuthValues.UserId;
-        //Debug.Log(test);
-        //Debug.Log("test");
+        Debug.Log("Joined room");
+        // string userId = PhotonNetwork.AuthValues.UserId;
+        // Debug.Log("userId: " + userId);
     }
+
+    // public override void OnDisconnected(DisconnectedCause cause)
+    // {
+    //     Debug.Log("Disconnected for reason " + cause.ToSctring());
+    // }
 
 }
